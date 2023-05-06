6534818dfe03bac7a23b0015a7653a678613323f
iyu88 <iyu88@naver.com>
2023-05-06 23:41:47 +0900

    feat: SVG 물리 엔진 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,92 @@
 'use client';
 
 import styles from './page.module.css'
-import { Engine, Render, Runner, Bodies, Composite, Svg, Vertices, Common } from "matter-js";
+import { Engine, Render, Runner, Bodies, Composite, Svg, Vertices, Common, Mouse, MouseConstraint } from "matter-js";
 import { useEffect } from 'react';
+import pathseg from 'pathseg';
+Common.setDecomp(require('poly-decomp'));
 
 export default function Home() {
 
     useEffect(() => {
-    const engine = Engine.create();
+        // create engine
+        const engine = Engine.create();
+        const world = engine.world;
 
-    const render = Render.create({ 
-        engine: engine,
-        element: document.querySelector('#canvas'),
-    })
+        // create renderer
+        const render = Render.create({
+            element: document.querySelector('#canvas'),
+            engine: engine,
+        });
 
-    const boxA = Bodies.rectangle(400, 200, 80, 80);
-    const boxB = Bodies.rectangle(450, 50, 80, 80);
-    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+        Render.run(render);
 
-    Composite.add(engine.world, [boxA, boxB, ground]);
+        // create runner
+        const runner = Runner.create();
+        Runner.run(runner, engine);
 
-    Render.run(render);
+        const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-    const runner = Runner.create();
+        // add bodies
+        if (typeof fetch !== 'undefined') {
+            const select = function(root, selector) {
+                return Array.prototype.slice.call(root.querySelectorAll(selector));
+            };
 
-    Runner.run(runner, engine);
+            const loadSvg = function(url) {
+                return fetch(url)
+                    .then(function(response) { return response.text(); })
+                    .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
+            };
+
+            ([
+                './1.svg',
+                './2.svg',
+                './3.svg',
+                './4.svg',
+                './5.svg',
+            ]).forEach(function(path, i) { 
+                loadSvg(path).then(function(root) {
+                    const color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+
+                    const vertexSets = select(root, 'path')
+                        .map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 10), 0.4, 0.4); });
+                    Composite.add(world, Bodies.fromVertices(100 + i * 150, 200 + i * 50, vertexSets, {
+                        render: {
+                            fillStyle: color,
+                            strokeStyle: color,
+                            lineWidth: 1
+                        }
+                    }, true));
+                });
+            });
+        } else {
+            Common.warn('Fetch is not available. Could not load SVG.');
+        }
+
+        Composite.add(world, [
+            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+            Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+        ]);
+
+        // add mouse control
+        const mouse = Mouse.create(render.canvas),
+            mouseConstraint = MouseConstraint.create(engine, {
+                mouse: mouse,
+                constraint: {
+                    stiffness: 0.2,
+                    render: {
+                        visible: false
+                    }
+                }
+            });
+
+        Composite.add(world, mouseConstraint);
+
+        // keep the mouse in sync with rendering
+        render.mouse = mouse;
 
     }, []);
 
