fcb00f594fc96c0d4500d975bba0fdcb7e80e916
iyu88 <iyu88@naver.com>
2023-05-06 23:32:11 +0900

    feat: matter.js 설치 및 데모 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
+'use client';
+
 import styles from './page.module.css'
+import { Engine, Render, Runner, Bodies, Composite, Svg, Vertices, Common } from "matter-js";
+import { useEffect } from 'react';
 
 export default function Home() {
+
+    useEffect(() => {
+    const engine = Engine.create();
+
+    const render = Render.create({ 
+        engine: engine,
+        element: document.querySelector('#canvas'),
+    })
+
+    const boxA = Bodies.rectangle(400, 200, 80, 80);
+    const boxB = Bodies.rectangle(450, 50, 80, 80);
+    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    Composite.add(engine.world, [boxA, boxB, ground]);
+
+    Render.run(render);
+
+    const runner = Runner.create();
+
+    Runner.run(runner, engine);
+
+    }, []);
+
     return (
     <main className={styles.main}>
-        Hello World
+        <div id="canvas"></div>
     </main>
     )
 }
