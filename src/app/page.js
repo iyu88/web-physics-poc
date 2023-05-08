b8c93c82f250596303d595b2e6c3b54d8e2fe375
iyu88 <iyu88@naver.com>
2023-05-08 23:01:16 +0900

    feat: SVG 파일마다 path를 여러 개 불러올 때

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import styles from './page.module.css'
-import { Engine, Render, Runner, Bodies, Composite, Svg, Vertices, Common, Mouse, MouseConstraint } from "matter-js";
+import { Engine, Render, Runner, Body, Bodies, Composite, Svg, Vertices, Common, Mouse, MouseConstraint } from "matter-js";
 import { useEffect } from 'react';
 import pathseg from 'pathseg';
 Common.setDecomp(require('poly-decomp'));
@@ -17,6 +17,10 @@ export default function Home() {
         const render = Render.create({
             element: document.querySelector('#canvas'),
             engine: engine,
+            options: {
+                background: '#ffffff',
+                wireframes: false
+            }
         });
 
         Render.run(render);
@@ -26,6 +30,13 @@ export default function Home() {
         Runner.run(runner, engine);
 
         const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+        Composite.add(world, [
+            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+            Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+        ]);
 
         // add bodies
         if (typeof fetch !== 'undefined') {
@@ -39,37 +50,28 @@ export default function Home() {
                     .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
             };
 
-            ([
-                './1.svg',
-                './2.svg',
-                './3.svg',
-                './4.svg',
-                './5.svg',
-            ]).forEach(function(path, i) { 
-                loadSvg(path).then(function(root) {
-                    const color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+            const color = ['#000000', '#964b00'];
 
-                    const vertexSets = select(root, 'path')
-                        .map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 10), 0.4, 0.4); });
-                    Composite.add(world, Bodies.fromVertices(100 + i * 150, 200 + i * 50, vertexSets, {
-                        render: {
-                            fillStyle: color,
-                            strokeStyle: color,
-                            lineWidth: 1
-                        }
-                    }, true));
-                });
-            });
-        } else {
-            Common.warn('Fetch is not available. Could not load SVG.');
+            Promise.all(['./6.svg', './6.svg', './6.svg','./6.svg','./6.svg',].map(loadSvg)).then(svgs => 
+                svgs.map((svg, i) => 
+                    select(svg, 'path')
+                        .map((path, j) =>
+                            Bodies.fromVertices(100 + 1 * 150, 200 - j * 40, 
+                            Vertices.scale(Svg.pathToVertices(path, 10), 0.4, 0.4),
+                            {
+                                render: {
+                                    fillStyle: color[j],
+                                    strokeStyle: color[j],
+                                    lineWidth: 2,
+                                    isStatic: true,
+                                    }
+                            }, true)
+                        )
+                    ))
+                    .then((shapes) => shapes.map(shape => {
+                        Composite.add(world, Body.create({parts: shape.flat()}));
+                    }))
         }
-
-        Composite.add(world, [
-            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-            Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-        ]);
 
         // add mouse control
         const mouse = Mouse.create(render.canvas),
